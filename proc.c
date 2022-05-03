9ab346d06535e8efb7a2f26b1d9c0dc3210caffc
Roham <rohamhayedi@gmail.com>
2022-05-03 22:06:54 +0430

    feat: add thread creator

--- proc.c
+++ proc.c
@@ -89,6 +89,9 @@ found:
   p->state = EMBRYO;
   p->pid = nextpid++;
   p->ctime = ticks;
+  p->isthread = 0;
+  p->thread_count = 1;
+
 
   release(&ptable.lock);
 
@@ -220,6 +223,64 @@ fork(void)
   release(&ptable.lock);
 
   return pid;
+}
+
+int thread_create(void *stack) {
+  
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  np->isthread = 1;
+  np->stack_top = (int) ((char*) stack + PGSIZE);
+
+  acquire(&ptable.lock);
+  np->pgdir = curproc->pgdir;
+  np->sz = curproc->sz;
+  release(&ptable.lock);
+
+  
+
+  // int stack_size = 
+
+  // Copy process state from proc.
+  // if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+  //   kfree(np->kstack);
+  //   np->kstack = 0;
+  //   np->state = UNUSED;
+  //   return -1;
+  // }
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+
+  // Clear %eax so that thread_create returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+  
+  curproc->thread_count++;
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+
+  np->state = RUNNABLE;
+
+  release(&ptable.lock);
+
+  return pid;
+
 }
 
 // Exit the current process.  Does not return.
